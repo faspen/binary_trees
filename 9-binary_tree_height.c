8e6d1b76a1d17a1c762d18525d7ee0e9fc78e778
Justin4587 <Thurman@atlasok.com>
2021-03-11 14:10:53 -0600

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,22 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - check for root
+ * binary_tree_height - check for root
  * @tree: node to check
  * Return: 1 if leaf, 0 if not
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t reight, leight;
+	size_t reight, leight;
 
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
-    reight = binary_tree_height(tree->right);
-    leight = binary_tree_height(tree->left);
+	reight = binary_tree_height(tree->right);
+	leight = binary_tree_height(tree->left);
 
-    if (reight > leight)
-        return (reight + 1);
-    else
-        return (leight + 1);
+	if (reight > leight)
+		return (reight + 1);
+	else
+		return (leight + 1);
 }
