05879d9dc9f3cb2cdefcc9d3fd7e0d26f790eec4
Justin4587 <Thurman@atlasok.com>
2021-03-11 14:14:37 -0600

    11 betty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - check for root
+ * binary_tree_size - check for root
  * @tree: node to check
  * Return: 1 if leaf, 0 if not
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
-        return (0);
+		return (0);
 
-    return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
