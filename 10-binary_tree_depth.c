8315b1c139df634bcfcaff6104753332b9a08d76
Justin4587 <Thurman@atlasok.com>
2021-03-11 14:12:38 -0600

    10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,22 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - check for root
+ * binary_tree_depth - check for root
  * @tree: node to check
  * Return: 1 if leaf, 0 if not
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t i = 0;
+	size_t i = 0;
 
 	if (!tree)
-        return (0);
+		return (0);
 
-    while (tree->parent)
-    {
-        i++;
-        tree = tree->parent;
-    }
+	while (tree->parent)
+	{
+		i++;
+		tree = tree->parent;
+	}
 
-    return (i);
+	return (i);
 }
